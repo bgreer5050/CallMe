790bc7fc65343e32812e2b1777f1b0a0852bdf92
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-03 18:10:08 -0500

    Misc commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PrintManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCallCell = new System.Windows.Forms.Button();
+            this.btnCallOffice = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnCallCell
+            // 
+            this.btnCallCell.Location = new System.Drawing.Point(61, 60);
+            this.btnCallCell.Name = "btnCallCell";
+            this.btnCallCell.Size = new System.Drawing.Size(75, 55);
+            this.btnCallCell.TabIndex = 0;
+            this.btnCallCell.Text = "Print to Cloud";
+            this.btnCallCell.UseVisualStyleBackColor = true;
+            this.btnCallCell.Click += new System.EventHandler(this.btnCallCell_Click);
+            // 
+            // btnCallOffice
+            // 
+            this.btnCallOffice.Location = new System.Drawing.Point(142, 60);
+            this.btnCallOffice.Name = "btnCallOffice";
+            this.btnCallOffice.Size = new System.Drawing.Size(75, 55);
+            this.btnCallOffice.TabIndex = 1;
+            this.btnCallOffice.Text = "Print to Office";
+            this.btnCallOffice.UseVisualStyleBackColor = true;
+            this.btnCallOffice.Click += new System.EventHandler(this.btnCallOffice_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(91, 134);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(112, 134);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(133, 134);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(154, 134);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(15, 14);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(175, 134);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(15, 14);
+            this.checkBox5.TabIndex = 6;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnCallOffice);
+            this.Controls.Add(this.btnCallCell);
+            this.Name = "Form1";
+            this.Text = "Print Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCallCell;
+        private System.Windows.Forms.Button btnCallOffice;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Twilio;
+
+namespace PrintManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallCell_Click(object sender, EventArgs e)
+        {
+            MakeCall("18154120866");
+        }
+
+        private void btnCallOffice_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(MakeCall("17089101012").ToString());
+        }
+
+        private bool MakeCall(string strPhone)
+        {
+            string strCallTo = strPhone;
+
+            throw new NotImplementedException("PUT IN ACCOUNTSID AND AUTH TOKEN INFO");
+            string AccountSid = "";
+            string AuthToken = "";
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+            // Build the parameters 
+            var options = new CallOptions();
+            options.To = strCallTo;
+            options.From = "+17086757020";
+            options.Url = "http://ositospartyrentals.com/Home/CallMindyWork";
+            options.Method = "GET";
+            options.FallbackMethod = "GET";
+            options.StatusCallbackMethod = "GET";
+            options.Record = false;
+
+            var call = twilio.InitiateOutboundCall(options);
+            if(call.Sid != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
